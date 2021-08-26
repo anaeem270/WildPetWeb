3dd6f889d800d861dad333698c0bea40f732cfea
JasonMarquez8 <88029339+JasonMarquez8@users.noreply.github.com>
2021-08-26 18:54:54 +1000

    Add files via upload

---
+++ Website/functions/main.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyDlyO_jXSow9X8plwNANgYoEDjauUqp2hg",
+    authDomain: "website-b114e.firebaseapp.com",
+    projectId: "website-b114e",
+    storageBucket: "website-b114e.appspot.com",
+    messagingSenderId: "1024995777553",
+    appId: "1:1024995777553:web:8f3f4075445ebf790e25b0"
+}
+
+firebase.initializeApp(config);
+
+const firestore= firebase.firebase();
